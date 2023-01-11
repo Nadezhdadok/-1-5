ddd4caedeec9781927c34bcf222228710e64c166
homeworkdome <homeworkdome@outlook.com>
2023-01-11 11:30:08 +0300

    Добавьте файлы проекта.

---
+++ Задача 1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 7 произвольных чисел");
+int[] array = new int[7];
+int s = 0;
+int k = 0;
+for (int i = 0;i < 7; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    s += array[i];
+    k++;
+}
+Console.WriteLine("Среднее арифметическое данных чисел:");
+Console.WriteLine(((double)s) / k);
